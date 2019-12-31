fb8b14ad9048f57295b7545aa384480fd63da798
Jeisson Sánchez <jeisson.fabian.96@gmail.com>
2019-12-31 01:19:34 -0500

    dia 4

---
+++ build/precache-manifest.cacbfd3278127c5f1ae9fed9662d4f17.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710cc9895dba3a6ad64a22ccdacbd2a1",
+    "url": "/munum-neo/index.html"
+  },
+  {
+    "revision": "d8c2dcefb1338fdf1b60",
+    "url": "/munum-neo/static/css/main.f707d842.chunk.css"
+  },
+  {
+    "revision": "3e6f4ec96b86033e4f8a",
+    "url": "/munum-neo/static/js/2.c84c4b96.chunk.js"
+  },
+  {
+    "revision": "e78dfdab5c41096b24065d8ab6322e32",
+    "url": "/munum-neo/static/js/2.c84c4b96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8c2dcefb1338fdf1b60",
+    "url": "/munum-neo/static/js/main.6b39c39e.chunk.js"
+  },
+  {
+    "revision": "59b9004e138f56a3a97c",
+    "url": "/munum-neo/static/js/runtime-main.5bfb6d4e.js"
+  },
+  {
+    "revision": "cef63d6377dfe52a8d779a3dd4c45fb6",
+    "url": "/munum-neo/static/media/LemonMilk.cef63d63.otf"
+  },
+  {
+    "revision": "e7aaeb37c332316239864536baacda40",
+    "url": "/munum-neo/static/media/campus2.e7aaeb37.jpg"
+  },
+  {
+    "revision": "0bed2bac4b2250fc0294c238f961a84f",
+    "url": "/munum-neo/static/media/colombianos-cover.0bed2bac.jpg"
+  },
+  {
+    "revision": "5e8c09cc11f4c8db8eb41d657d7010c0",
+    "url": "/munum-neo/static/media/diego4.5e8c09cc.jpg"
+  },
+  {
+    "revision": "4aa034417baada4d71e9a9f56d4d0cb3",
+    "url": "/munum-neo/static/media/facebook.4aa03441.svg"
+  },
+  {
+    "revision": "f586f23eafce999c931ef0e771804b65",
+    "url": "/munum-neo/static/media/instagram.f586f23e.svg"
+  },
+  {
+    "revision": "0b6dde9d1f1a15c85865f8482165fcad",
+    "url": "/munum-neo/static/media/logo.0b6dde9d.svg"
+  },
+  {
+    "revision": "e5061f8a77875375c2a920d244fdcba4",
+    "url": "/munum-neo/static/media/mario2.e5061f8a.jpg"
+  },
+  {
+    "revision": "04f6e561656c7dd0812da63d69c4f9f0",
+    "url": "/munum-neo/static/media/onu-cover.04f6e561.jpg"
+  },
+  {
+    "revision": "0e0fd4ee4dccc287f517b195378956db",
+    "url": "/munum-neo/static/media/regionales-cover.0e0fd4ee.jpg"
+  },
+  {
+    "revision": "dcdf778db8dbca4b5ad51cbc64c1e576",
+    "url": "/munum-neo/static/media/santiago.dcdf778d.jpg"
+  }
+]);
